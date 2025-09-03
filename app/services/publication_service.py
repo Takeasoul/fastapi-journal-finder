af03615a492901e4bb8f9cc3b2065167ddb7eaab
Иванов Никита Александрович <ni.ivanov@bft.ru>
2025-09-03 22:02:22 +0300

    new endpoint

--- app/services/publication_service.py
+++ app/services/publication_service.py
@@ -319,43 +319,31 @@ async def get_paginated_publications_with_index_and_information(
     ).distinct()
 
     # Запрос на количество
-    count_query = select(func.count(distinct(Publication.id)))
+    count_query = select(func.count(distinct(Publication.id))).select_from(Publication)
 
-    # --- фильтры ---
     for key, value in filters.items():
         if not value:
             continue
-
         if key == "languages":
             lang_conditions = [Publication.language.like(f"%{lang.value}%") for lang in value]
-            base_query = base_query.where(or_(*lang_conditions))
             count_query = count_query.where(or_(*lang_conditions))
         elif key == "name":
-            base_query = base_query.where(Publication.name.ilike(f"%{value}%"))
             count_query = count_query.where(Publication.name.ilike(f"%{value}%"))
         elif key == "el_updated_at_from":
-            base_query = base_query.where(Publication.el_updated_at >= value)
             count_query = count_query.where(Publication.el_updated_at >= value)
         elif key == "el_updated_at_to":
-            base_query = base_query.where(Publication.el_updated_at <= value)
             count_query = count_query.where(Publication.el_updated_at <= value)
         elif key in enum_fields:
             try:
                 enum_value = enum_fields[key](value)
-                base_query = base_query.where(getattr(Publication, key) == enum_value)
                 count_query = count_query.where(getattr(Publication, key) == enum_value)
             except ValueError:
                 continue
         elif key == "actual_specialty":
-            if value:
-                base_query = base_query.join(Publication.actual_specialties).filter(
-                    ActualSpecialty.specialty_id.in_(value)
-                )
-                count_query = count_query.join(Publication.actual_specialties).filter(
-                    ActualSpecialty.specialty_id.in_(value)
-                )
+            count_query = count_query.outerjoin(Publication.actual_specialties).filter(
+                ActualSpecialty.specialty_id.in_(value)
+            )
         elif hasattr(Publication, key):
-            base_query = base_query.where(getattr(Publication, key) == value)
             count_query = count_query.where(getattr(Publication, key) == value)
 
     # --- пагинация ---
